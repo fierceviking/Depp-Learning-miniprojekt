0e126498883f2a4d797fe05c69e4cf9ac323d7fb
Mardaaa <martin_tp@live.dk>
2024-10-15 10:47:15 +0200

    added txt file

--- model.py
+++ model.py
@@ -1,1 +1,15 @@
-import torch
+import torch.nn as nn
+
+class DecomNet(nn.Module):
+    def __init__(self):
+        super(DecomNet).__init__()
+
+        # Define 5 conv layers (based on the R-Net article)
+
+
+class EnhanceNet(nn.Module):
+    pass
+
+class RetinexNet(nn.Module):
+    pass
+
