9d8dd1b11ff99f71a89c2b7cd7212c6d51633eb8
Mardaaa <martin_tp@live.dk>
2024-10-15 11:03:13 +0200

    check point

--- model.py
+++ model.py
@@ -1,11 +1,29 @@
 import torch.nn as nn
 
 class DecomNet(nn.Module):
-    def __init__(self):
+    def __init__(self, kernel_size=3):
         super(DecomNet).__init__()
+        padding = 1
+        # Define 5 conv layers (based on the R-Net article) and 2 layers without reLU
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv5 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv6 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv7 = nn.Conv2d(64, 3, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
 
-        # Define 5 conv layers (based on the R-Net article)
+        # Define ReLU
+        self.relu = nn.ReLU()
 
+    def forward(self, input):
+        # First layer
+        c1 = self.conv1(input)
+
+        # Five layers with ReLU
+        c2 = self.relu(self.conv2(c1))
+        c3 = self.relu(self.conv3(c2))
+        nn.ReLU()
 
 class EnhanceNet(nn.Module):
     pass
