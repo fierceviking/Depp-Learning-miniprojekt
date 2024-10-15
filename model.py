d05300ec8713a41fd5b8e2711dd61e100c77fa81
Mardaaa <martin_tp@live.dk>
2024-10-15 11:18:50 +0200

    added random tensor

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class DecomNet(nn.Module):
@@ -5,25 +6,36 @@ class DecomNet(nn.Module):
         super(DecomNet).__init__()
         padding = 1
         # Define 5 conv layers (based on the R-Net article) and 2 layers without reLU
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv5 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv6 = nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
-        self.conv7 = nn.Conv2d(64, 3, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
+        self.conv_first = nn.Conv2d(4, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate') # 4 channels: R, G, B, and illumination
 
-        # Define ReLU
-        self.relu = nn.ReLU()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=kernel_size, padding=padding, padding_mode='replicate'),
+            nn.ReLU()
+        )
+
+        self.conv_last = nn.Conv2d(64, 4, kernel_size=kernel_size, padding=padding, padding_mode='replicate')
 
     def forward(self, input):
-        # First layer
-        c1 = self.conv1(input)
+        # First layer (without ReLU)
+        c1 = self.conv_first(input)
 
         # Five layers with ReLU
-        c2 = self.relu(self.conv2(c1))
-        c3 = self.relu(self.conv3(c2))
-        nn.ReLU()
+        c2 = self.conv_layers(c1)
+
+        # Last layer (without ReLU)
+        c3 = self.conv_last(c2)
+        print(c3.shape)
+        # Output: Reflectance (RGB) and Illumination
+        # Recall tensor shape: (batch size, channels, height, width)
+        #R = torch.sigmoid(c3())
 
 class EnhanceNet(nn.Module):
     pass
@@ -31,3 +43,14 @@ class EnhanceNet(nn.Module):
 class RetinexNet(nn.Module):
     pass
 
+
+def main():
+    # Random tensor for testing
+    input_tensor = torch.rand(1, 4, 128, 128)
+
+    decom = DecomNet()
+    decom.forward(input_tensor)
+
+
+if __name__ == '__main__':
+    main()
